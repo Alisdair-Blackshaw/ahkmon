179485f2a97da48e30a8fce9bc5dff347a31bc28
SuperFirm84 <99459327+SuperFirm84@users.noreply.github.com>
2022-03-28 23:08:52 +0000

    Update compile_release.bat

--- compile_release.bat
+++ compile_release.bat
@@ -1,6 +1,6 @@
 @echo off
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "ahkmon.ahk" /icon "imgs/dqx.ico"
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "ahkmon_updater.ahk" /icon "imgs/updater.ico"
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "dialogFinder.ahk" /icon "imgs/dialog.ico"
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "questFinder.ahk" /icon "imgs/quest.ico"
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "ahkmon.ahk" /bin "C:\Program Files\AutoHotkey\Compiler\Unicode 32-bit.bin" /icon "imgs/dqx.ico"
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "ahkmon_updater.ahk" /bin "C:\Program Files\AutoHotkey\Compiler\Unicode 32-bit.bin" /icon "imgs/updater.ico"
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "dialogFinder.ahk" /bin "C:\Program Files\AutoHotkey\Compiler\Unicode 32-bit.bin" /icon "imgs/dialog.ico"
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "questFinder.ahk" /bin "C:\Program Files\AutoHotkey\Compiler\Unicode 32-bit.bin" /icon "imgs/quest.ico"
 "C:\Program Files\7-Zip\7z.exe" a -tzip ahkmon.zip ./imgs ahkmon.exe dqxtrl.db sqlite3.dll version ahkmon_updater.exe dialogFinder.exe questFinder.exe
